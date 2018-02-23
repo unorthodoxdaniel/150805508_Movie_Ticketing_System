4a49308e5225ea8282a20a2780145dae45023aab
unorthodoxdaniel <unorthodoxdaniel@gmail.com>
2018-02-23 19:45:38 +0100

    Select Location
    Adds a "Select Location" paragraph on mouse out and does it more than
    once (for every mouseout).
    Also adds a drop down menu for the actual locations, but only does this
    once.

--- ux.js
+++ ux.js
@@ -2,6 +2,42 @@ var name = document.getElementById("Name").addEventListener("click", function(){
     document.getElementById("Name").addEventListener("mouseout", afterName)
 });
 
+var container = document.getElementById("container");
+var form = document.getElementById("the-form");
+var getLocation = document.createElement("SELECT");
+var location1 = document.createElement("OPTION");
+var location2 = document.createElement("OPTION");
+var location3 = document.createElement("OPTION");
+var location4 = document.createElement("OPTION");
+location1.innerHTML = "Lagos";
+location1.addEventListener("click", afterLocation);
+
+location2.innerHTML = "Abuja";
+location2.addEventListener("click", afterLocation);
+
+location3.innerHTML = "Ibadan";
+location3.addEventListener("click", afterLocation);
+
+location4.innerHTML = "Port Harcourt";
+location4.addEventListener("click", afterLocation);
+
+
+
 function afterName(){
-    alert("Done!");
+    //alert("Done!");
+    var paragraph = document.createElement("p");
+    paragraph.innerHTML = "Select your location";
+
+    container.appendChild(paragraph);
+    getLocation.appendChild(location1);
+    getLocation.appendChild(location2);
+    getLocation.appendChild(location3);
+    getLocation.appendChild(location4);
+    container.appendChild(getLocation);
+
+    //Should probably check to see if the value is changed before I do all of this.
+}
+
+function afterLocation(){
+
 }
